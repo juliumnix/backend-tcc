9b158edfd40e2277bbcfb3913a31d9d40771578d
juliumnix <juliumnix@gmail.com>
2023-09-28 16:06:47 -0300

    ajustando cors

--- src/main/kotlin/com/udesc/reactflutternativeAndroid/controller/AvailableAPI.kt
+++ src/main/kotlin/com/udesc/reactflutternativeAndroid/controller/AvailableAPI.kt
@@ -1,12 +1,14 @@
 package com.udesc.reactflutternativeAndroid.controller
 
 import com.udesc.reactflutternativeAndroid.model.responses.StatusResponse
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 class AvailableAPI {
 
     @GetMapping("/available")
